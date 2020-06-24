3e060321eb5c72cfd68824b164efa6c5d3290571
Don Mallory <dmm333@gmail.com>
2020-06-24 16:47:02 -0400

    solution added

--- solution.js
+++ solution.js
@@ -3,5 +3,22 @@
 //return true if the words are anagrams of each other
 //return false if they are not
 function checkIsAnagram(word, baseline) {
+    const counts = {}
+    if (baseline.length !== word.length) {
+        return false
+    }
 
+    [...word].forEach(char=>counts[char] = (counts[char]||0) + 1);
+    
+    [...baseline].forEach(char=> {
+        if (counts[char] <= 0) {
+            return false
+        } else {
+            counts[char]--
+        }
+    });
+    if (Object.values(counts).every(val=>val === 0)) {
+        return true
+    }
+    return false
 }
